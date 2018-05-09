af7e79e93756d8b4279b02c66dd8c4658ba47280
Amanti Lulo <amanti.lulo@gov.local>
2018-05-09 08:36:13 +0200

    added some animations. should llok nice in theory

---
+++ map.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([41.32615001525243, 19.818005561828613], 15);
+
+var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
